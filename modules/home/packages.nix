14c70739c2bb946670adc240fa7127064d4d9362
sxmchn <johndoe@mac-mini.com>
2026-05-12 22:11:19 +0300

    add some packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -23,6 +23,11 @@
     # On ubuntu, we need this less for `man home-configuration.nix`'s pager to
     # work.
     less
+
+    ffmpeg
+    
+    # Network
+    inetutils 
   ];
 
   # Programs natively supported by home-manager.
