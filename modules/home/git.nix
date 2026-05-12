3b17cda1722fde0656686276f16edace0f96720e
sxmchn <johndoe@mac-mini.com>
2026-05-12 21:15:20 +0300

    Update git nix parameter structure

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,24 +1,22 @@
 { config, ... }:
 {
-  home.shellAliases = {
-    g = "git";
-    lg = "lazygit";
-  };
-
-  # https://nixos.asia/en/git
   programs = {
     git = {
       enable = true;
       ignores = [ "*~" "*.swp" ];
       settings = {
-        user.name = config.me.fullname;
-        user.email = config.me.email;
-        alias.ci = "commit";
+        user = {
+          name = config.me.fullname;
+          email = config.me.email;
+        };
+        alias = {
+          ci = "commit";
+        };
         # init.defaultBranch = "master";
-        # pull.rebase = "false";
+        # pull.rebase = false;
       };
     };
+
     lazygit.enable = true;
   };
-
 }
